0fc64419b5f8e11b6efc6f7e98537bdfcbbeb62a
RomanKozeko <r.kozeko@gmail.com>
2018-05-11 14:39:01 +0300

    add devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  #get "users#index"
+  resources :users, only: [:index, :show]
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :posts
   root to: "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
